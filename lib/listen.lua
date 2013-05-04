7153d8e6e1d1584a96f94dba8e4fad022bc4f8dc
tie372 <tie.372@gmail.com>
2013-05-04 17:40:46 -0600

    Forgot to save a few files before committing.

--- lib/listen.lua
+++ lib/listen.lua
@@ -2,6 +2,10 @@ local net = require('net')
 
 local listen = {}
 
+
+-- Creates a server listening on port.
+-- Once the server is started, executes callback with port
+-- as its single parameter.
 function listen:listenOn(port, callback)
   listen.server = net.createServer(function(client)
     -- 
@@ -12,14 +16,23 @@ function listen:listenOn(port, callback)
   end)
 end
 
+
+-- Asynchronously returns the port the listen server is listening on.
+-- If the listen server is not listening, it will first start the listen server.
+-- The port the server listens on (in the case where the server is not
+-- currently listening) can be specified by the port parameter.
 function listen:getPort(callback, port)
   port = port or 6881
   if not listen.server then return listen:listenOn(port, callback) end
   return callback(listen.server:address().port)
 end
 
+
+-- Synchronously eturns the port the listen server is listening on,
+-- or nil if it isn't listening on a port.
 function listen:getPortSync()
   if listen.server then return listen.server:address().port end
 end
+
 
 return listen
--- lib/torrent.lua
+++ lib/torrent.lua
@@ -10,18 +10,26 @@ local Torrent = Object:extend()
 
 
 -- Creates a new Torrent from the .torrent file located at location.
+-- Most operations are lazy -- that is it won't attempt to read/download the file
+-- until you call start.
 function Torrent:initialize(location)
   self.location = location
 end
 
 
--- 
+-- Destroys the torrent gracefully, closing any connections and writing out any
+-- data.
 function Torrent:destroy()
-
+  --
 end
 
 
--- 
+-- Asynchronously reads the metainfo from the .torrent file.
+-- If the first 4 characters of location are 'http', then
+-- an http get request will be used to retrieve the file.
+-- Otherwise, it is assumed to be on the local filesystem.
+-- After reading the file, the bencoded dictionary is parsed.
+-- The callback has no arguments.
 function Torrent:readMetainfo(callback)
   if self.metainfo then return nil end
   
@@ -31,13 +39,11 @@ function Torrent:readMetainfo(callback)
       local data = ''
 
       res.setEncoding('utf8')
-      res.on('data', function(chunk)
-        --
-      end)
+      res.on('data', function(chunk) data = data .. chunk end)
       
       res.on('end', function()
-        --
-        print(data)
+        self.metainfo = bencode.decode(data)
+        if callback then callback() end
       end)
     end)
   else
@@ -49,15 +55,15 @@ function Torrent:readMetainfo(callback)
 end
 
 
--- 
-function Torrent:initTrackers()
-  --if not self.metainfo then self:readMetainfo() end
+-- Creates tracker objects for each url in the announce/announceList
+-- portions of the metainfo dictionary.
+function Torrent:initTrackers()=
   if self.trackers then return nil end
   self.trackers = {}
 end
 
 
--- 
+-- Starts or resumes the torrent.
 function Torrent:start()
   -- This will have to be restructured for asynchronousness.
   if not self.metainfo then self:readMetainfo() end
@@ -71,13 +77,16 @@ function Torrent:start()
 end
 
 
--- 
+-- Stops the torrent.  It can be started again by calling start.
 function Torrent:stop()
 
 end
 
 
--- 
+-- Announces this torrent to the given tracker.  An optional event
+-- specifies any event that has taken place ("started", "stopped",
+-- or "completed").  After the announce is complete, callback is
+-- executed with the parsed response.
 function Torrent:announce(tracker, event, callback)
   local options = {
     infoHash = self.metainfo.infoHash,
