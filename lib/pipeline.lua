8b49206f7ed33cb7c3ad87b994c6f5841d647f51
tie372 <tie.372@gmail.com>
2013-05-07 16:09:52 -0600

    Improvements to pipeline.
    There may be some inefficiencies/room for improvement, but premature optimization and all that.

--- lib/pipeline.lua
+++ lib/pipeline.lua
@@ -1,6 +1,8 @@
+local table = require('table')
+
 local timer = require('timer')
 
-local Object = require('core').Object
+local Emitter = require('core').Emitter
 
 local Pipeline = Emitter:extend()
 
@@ -11,14 +13,52 @@ function Pipeline:initialize(size)
   self.next = function(self) local x = self.queue[1] table.remove(self.queue, 1) return x end
 end
 
-local function fill(self)
+function Pipeline:fill()
   while #self.pending < self.size do
     if #self.queue == 0 then break end -- We don't have anything to add to the pipeline.
     
     local job = self:next()
-    job.run(job.obj, job.done)
+    table.insert(self.pending, job)
     
-    if job.maxlife and job.maxlife > 0 then timer.setTimeout(job.maxlife, job.death) end
+    job.run(job.obj, function(...)
+      
+      if job.dead then return end
+      
+      -- Clear an expriation timeout if there is one.
+      if job.deathTimer then timer.clearTimer(job.deathTimer) end
+      
+      -- Run the callback.
+      if job.done then job.done(...) end
+      
+      -- Remove the job after it's finished.  Hate to do a linear pass here.
+      local i
+      for i = 1, #self.pending do
+        if self.pending[i] == job then
+          table.remove(self.pending, i)
+          break
+        end
+      end
+      
+      -- Start more jobs.
+      self:fill()
+    end)
+    
+    if job.maxlife and job.maxlife > 0 then
+      job.deathTimer = timer.setTimeout(job.maxlife, function()
+        if job.death then job.death() end
+        
+        for i = 1, #self.pending do
+          if self.pending[i] == job then
+            table.remove(self.pending, i)
+            break
+          end
+        end
+        
+        self:fill()
+        
+        job.dead = true
+      end)
+    end
   end
 end
 
@@ -28,7 +68,8 @@ function Pipeline:add(obj, run, done, maxlife, death)
     run = run,
     done = done,
     maxlife = maxlife,
-    death = death
+    death = death,
+    deathTimer = nil
   }
   
   self:fill()
--- test.lua
+++ test.lua
@@ -2,9 +2,5 @@ local Torrent = require('./torrent')
 
 -- Place a torrent named sample.torrent in the same directory
 -- as this file.
--- Parse its metainfo with:
---   luvit test.lua
--- We are not outputting the info dictionary because it
--- contains a large amount of binary data.
 local t = Torrent:new('sample.torrent')
 t:start()
